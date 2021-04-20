fbf31014c50d1578ecca392a3db561628d223242
Mark <mba@crtweb.ru>
2021-04-20 19:30:28 +0500

    added little table

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -3,7 +3,7 @@ const version = 'v2'
 
 const endpoints = {
   products: {
-    getProduct: `/${version}/beers/random`
+    getProduct: `/${version}/beers`
   }
 }
 
--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -2,8 +2,17 @@ import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchProductsStartAsync, clearProducts } from '../../redux/products/productsSlice'
 import { selectGetProducts } from '../../redux/products/selector'
+import { makeStyles, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@material-ui/core'
+
+const useStyles = makeStyles({
+  tableImg: {
+    width: 60,
+    height: 'auto',
+  },
+})
 
 const Catalog = () => {
+  const classes = useStyles()
   const dispatch = useDispatch()
   const [products, setProducts] = useState([])
 
@@ -25,21 +34,70 @@ const Catalog = () => {
     }
   }, [selectedProducts])
 
+  const columns = [
+    {
+      field: 'id',
+      headName: 'ID',
+      id: 0,
+      minWidth: 170,
+    },
+    {
+      field: 'date',
+      headName: 'Date',
+      id: 1,
+      minWidth: 170,
+    },
+    {
+      field: 'name',
+      headName: 'Name',
+      id: 2,
+      minWidth: 170,
+    },
+    {
+      field: 'image',
+      headName: 'View',
+      id: 3,
+      minWidth: 170,
+    }
+  ]
+      // rows={products} columns={columns} pageSize={5} checkboxSelection />
   return (
     <div>
-      {(products.length && products?.map((item, key) => (
-          <span key={key.toString()}>
-            {item.name}
-            <img
-              alt=""
-              src={item?.image_url}
-            />
-          </span>
-        ))) || (
-        <span>
-          Это каталог
-        </span>
-      )}
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              {(columns?.map((column) => (
+              <TableCell
+                key={column.id}
+                style={{ minWidth: column.minWidth }}
+              >
+                {column.headName}
+              </TableCell>
+              ))) || null}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+          {(products?.map((row, key) => (
+            <TableRow key = {row.id}>
+              <TableCell>
+                {row?.id}
+              </TableCell>
+              <TableCell>
+                {row?.first_brewed}
+              </TableCell>
+              <TableCell>
+                {row?.name}
+              </TableCell>
+              <TableCell>
+                <img className={classes.tableImg} src={row?.image_url} alt="Картинки нет((" />
+              </TableCell>
+            </TableRow>
+          )))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      
     </div>
   )
 }
